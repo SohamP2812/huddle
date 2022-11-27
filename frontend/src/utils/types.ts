c3b81ab020c3b71e684a673851b77858a86ea37a
Soham Parmar <soham.parmar@faire.com>
2022-11-27 15:18:17 -0700

    migrate to RTK query - types file

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,49 @@
+export interface User {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  isManager?: boolean;
+  createdAt: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface AccountCreationInfo {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  manager: User;
+  sport: string;
+  createdAt: string;
+}
+
+export interface Event {
+  id: number;
+  name: string;
+  team: Team;
+  startTime: string;
+  endTime: string;
+  eventType: string;
+  teamScore: number;
+  opponentScore: number;
+  createdAt: string;
+}
+
+export interface Participant {
+  id: number;
+  user: User;
+  event: Event;
+  attendance: string;
+}
