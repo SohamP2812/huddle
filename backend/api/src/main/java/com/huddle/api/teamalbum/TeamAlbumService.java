d57f91ead02c036df6a928cf3d7664a50723b51e
Soham Parmar <soham.parmar@faire.com>
2023-06-29 14:16:04 -0600

    Convert TeamAlbumService to use new transactor

--- backend/api/src/main/java/com/huddle/api/teamalbum/TeamAlbumService.java
+++ backend/api/src/main/java/com/huddle/api/teamalbum/TeamAlbumService.java
@@ -2,45 +2,52 @@ package com.huddle.api.teamalbum;
 
 import com.huddle.api.team.DbTeam;
 import com.huddle.api.team.TeamService;
+import com.huddle.core.persistence.Transactor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
 public class TeamAlbumService {
-    @Autowired
-    TeamAlbumRepository teamAlbumRepository;
 
     @Autowired
     TeamService teamService;
+
+    @Autowired
+    Transactor transactor;
 
     public DbTeamAlbum createAlbum(
             TeamAlbumRequest teamAlbumRequest,
             Long teamId
     ) {
-        DbTeam dbTeam = teamService.getTeam(teamId);
-
-        DbTeamAlbum dbTeamAlbum = new DbTeamAlbum(
-                teamAlbumRequest.getName(),
-                dbTeam
+        return transactor.call(session -> {
+                    DbTeam dbTeam = teamService.getTeam(teamId);
+                    return session.save(
+                            new DbTeamAlbum(
+                                    teamAlbumRequest.getName(),
+                                    dbTeam
+                            )
+                    );
+                }
         );
-
-        return teamAlbumRepository.save(dbTeamAlbum);
     }
 
     public List<DbTeamAlbum> getAlbums(Long teamId) {
-        DbTeam dbTeam = teamService.getTeam(teamId);
+        return transactor.call(session -> {
+                    DbTeam dbTeam = teamService.getTeam(teamId);
 
-        return dbTeam.getAlbums()
-                .stream()
-                .toList();
+                    return dbTeam.getAlbums()
+                            .stream()
+                            .toList();
+                }
+        );
     }
 
 
     public DbTeamAlbum getAlbum(Long albumId) {
-        return teamAlbumRepository.findById(albumId)
-                .orElseThrow(() -> new EntityNotFoundException("No album exists with this id."));
+        return transactor.call(session ->
+                session.get(DbTeamAlbum.class, albumId)
+        );
     }
 }
