5b29204801aa2ffdfbd9102838a8875854554791
Soham Parmar <soham.parmar@faire.com>
2023-06-29 14:15:29 -0600

    Convert TeamMemberService to use new transactor

--- backend/api/src/main/java/com/huddle/api/teammember/TeamMemberService.java
+++ backend/api/src/main/java/com/huddle/api/teammember/TeamMemberService.java
@@ -1,28 +1,26 @@
 package com.huddle.api.teammember;
 
 import com.huddle.api.event.DbEvent;
-import com.huddle.api.eventparticipant.EventParticipantRepository;
+import com.huddle.api.eventparticipant.EventParticipantService;
 import com.huddle.api.team.DbTeam;
 import com.huddle.api.team.TeamService;
 import com.huddle.api.user.DbUser;
 import com.huddle.api.user.UserService;
 import com.huddle.core.email.EmailSender;
 import com.huddle.core.exceptions.BadRequestException;
+import com.huddle.core.persistence.Transactor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class TeamMemberService {
-    @Autowired
-    TeamMemberRepository teamMemberRepository;
 
     @Autowired
-    EventParticipantRepository eventParticipantRepository;
+    EventParticipantService eventParticipantService;
 
     @Autowired
     TeamService teamService;
@@ -33,12 +31,18 @@ public class TeamMemberService {
     @Autowired
     EmailSender emailSender;
 
-    public List<DbTeamMember> getMembers(Long teamId) {
-        DbTeam dbTeam = teamService.getTeam(teamId);
+    @Autowired
+    Transactor transactor;
 
-        return dbTeam.getTeamMembers()
-                .stream()
-                .toList();
+    public List<DbTeamMember> getMembers(Long teamId) {
+        return transactor.call(session -> {
+                    DbTeam dbTeam = teamService.getTeam(teamId);
+
+                    return dbTeam.getTeamMembers()
+                            .stream()
+                            .toList();
+                }
+        );
     }
 
     public DbTeamMember addMember(
@@ -46,83 +50,97 @@ public class TeamMemberService {
             Long userId,
             EPosition position
     ) {
-        DbTeam dbTeam = teamService.getTeam(teamId);
+        return transactor.call(session -> {
+                    DbTeam dbTeam = teamService.getTeam(teamId);
 
-        DbUser dbUserToAdd = userService.getUser(userId);
+                    DbUser dbUserToAdd = userService.getUser(userId);
 
-        List<DbTeam> dbTeams = dbUserToAdd.getMemberTeams()
-                .stream()
-                .map(DbTeamMember::getTeam)
-                .toList();
+                    List<DbTeam> dbTeams = dbUserToAdd.getMemberTeams()
+                            .stream()
+                            .map(DbTeamMember::getTeam)
+                            .toList();
 
-        if (dbTeams.contains(dbTeam)) {
-            throw new BadRequestException("User is already a member of this team.");
-        }
+                    if (dbTeams.contains(dbTeam)) {
+                        throw new BadRequestException("User is already a member of this team.");
+                    }
 
-        DbTeamMember dbTeamMember = new DbTeamMember(
-                ERole.ROLE_MEMBER,
-                position,
-                dbUserToAdd,
-                dbTeam
+                    DbTeamMember dbTeamMember = new DbTeamMember(
+                            ERole.ROLE_MEMBER,
+                            position,
+                            dbUserToAdd,
+                            dbTeam
+                    );
+
+                    session.save(dbTeamMember);
+
+                    Map<String, Object> variables = new HashMap<>();
+                    variables.put("name", dbUserToAdd.getFirstName());
+                    variables.put("teamName", dbTeam.getName());
+                    variables.put("managerName", dbTeam.getManager().getFirstName());
+                    variables.put("managerEmail", dbTeam.getManager().getEmail());
+
+                    emailSender.sendNow(
+                            dbUserToAdd.getEmail(),
+                            "AddedToTeam",
+                            variables,
+                            String.format("You've Been Added to the %s Team!", dbTeam.getName())
+                    );
+
+                    return dbTeamMember;
+                }
         );
-
-        teamMemberRepository.save(dbTeamMember);
-
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("name", dbUserToAdd.getFirstName());
-        variables.put("teamName", dbTeam.getName());
-        variables.put("managerName", dbTeam.getManager().getFirstName());
-        variables.put("managerEmail", dbTeam.getManager().getEmail());
-
-        emailSender.sendNow(
-                dbUserToAdd.getEmail(),
-                "AddedToTeam",
-                variables,
-                String.format("You've Been Added to the %s Team!", dbTeam.getName())
-        );
-
-        return dbTeamMember;
     }
 
     public DbTeamMember getMember(
             Long teamId,
-            Long userId
+            Long memberId
     ) {
-        return teamMemberRepository.findByTeamIdAndMemberId(
-                teamId,
-                userId
-        ).orElseThrow(() -> new EntityNotFoundException("This user is not a member of that team."));
+        return transactor.call(session ->
+                session.createCriteria(DbTeamMember.class)
+                        .addEq("team.id", teamId)
+                        .addEq("member.id", memberId)
+                        .uniqueResult()
+        );
     }
 
     public void deleteMember(
             Long teamId,
             Long userId
     ) {
-        DbTeam dbTeam = teamService.getTeam(teamId);
+        transactor.call(session -> {
+                    DbTeam dbTeam = teamService.getTeam(teamId);
 
-        DbTeamMember teamMember = getMember(teamId, userId);
+                    DbTeamMember teamMember = getMember(teamId, userId);
 
-        if (teamMember.isManager()) {
-            throw new BadRequestException("You cannot delete the manager from a team.");
-        }
+                    if (teamMember.isManager()) {
+                        throw new BadRequestException("You cannot delete the manager from a team.");
+                    }
 
-        teamMemberRepository.delete(teamMember);
+                    session.delete(teamMember);
 
-        for (DbEvent dbEvent : dbTeam.getEvents()) {
-            eventParticipantRepository.deleteByParticipantIdAndEventId(userId, dbEvent.getId());
-        }
+                    for (DbEvent dbEvent : dbTeam.getEvents()) {
+                        eventParticipantService.deleteByParticipantIdAndEventId(
+                                session,
+                                userId,
+                                dbEvent.getId()
+                        );
+                    }
 
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("name", teamMember.getMember().getFirstName());
-        variables.put("teamName", dbTeam.getName());
-        variables.put("managerName", dbTeam.getManager().getFirstName());
-        variables.put("managerEmail", dbTeam.getManager().getEmail());
+                    Map<String, Object> variables = new HashMap<>();
+                    variables.put("name", teamMember.getMember().getFirstName());
+                    variables.put("teamName", dbTeam.getName());
+                    variables.put("managerName", dbTeam.getManager().getFirstName());
+                    variables.put("managerEmail", dbTeam.getManager().getEmail());
 
-        emailSender.sendNow(
-                teamMember.getMember().getEmail(),
-                "RemovedFromTeam",
-                variables,
-                String.format("You've Been Removed from the %s Team", dbTeam.getName())
+                    emailSender.sendNow(
+                            teamMember.getMember().getEmail(),
+                            "RemovedFromTeam",
+                            variables,
+                            String.format("You've Been Removed from the %s Team", dbTeam.getName())
+                    );
+
+                    return true;
+                }
         );
     }
 
@@ -131,13 +149,15 @@ public class TeamMemberService {
             Long userId,
             UpdateMemberRequest updateMemberRequest
     ) {
-        DbTeamMember dbTeamMember = getMember(
-                teamId,
-                userId
+        return transactor.call(session -> {
+                    DbTeamMember dbTeamMember = getMember(
+                            teamId,
+                            userId
+                    );
+
+                    dbTeamMember.setPosition(updateMemberRequest.getPosition());
+                    return session.update(dbTeamMember);
+                }
         );
-
-        dbTeamMember.setPosition(updateMemberRequest.getPosition());
-
-        return teamMemberRepository.save(dbTeamMember);
     }
 }
