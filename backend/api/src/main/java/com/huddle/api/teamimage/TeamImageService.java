1befa68fbc8028d20872dbe3449afd65c1a1587c
Soham Parmar <soham.parmar@faire.com>
2023-06-29 14:15:43 -0600

    Convert TeamImageService to use new transactor

--- backend/api/src/main/java/com/huddle/api/teamimage/TeamImageService.java
+++ backend/api/src/main/java/com/huddle/api/teamimage/TeamImageService.java
@@ -2,6 +2,7 @@ package com.huddle.api.teamimage;
 
 import com.huddle.api.teamalbum.DbTeamAlbum;
 import com.huddle.api.teamalbum.TeamAlbumService;
+import com.huddle.core.persistence.Transactor;
 import com.huddle.core.storage.StorageProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,34 +13,41 @@ import java.util.List;
 @Service
 public class TeamImageService {
     @Autowired
-    TeamImageRepository teamImageRepository;
-
-    @Autowired
     TeamAlbumService teamAlbumService;
 
     @Autowired
     StorageProvider storageProvider;
 
-    public List<DbTeamImage> getImages(Long albumId) {
-        DbTeamAlbum dbTeamAlbum = teamAlbumService.getAlbum(albumId);
+    @Autowired
+    Transactor transactor;
 
-        return dbTeamAlbum.getImages()
-                .stream()
-                .toList();
+    public List<DbTeamImage> getImages(Long albumId) {
+        return transactor.call(session -> {
+                    DbTeamAlbum dbTeamAlbum = teamAlbumService.getAlbum(albumId);
+
+                    return dbTeamAlbum.getImages()
+                            .stream()
+                            .toList();
+                }
+        );
+
     }
 
     public DbTeamImage addImage(MultipartFile image, Long teamId, Long albumId) {
-        DbTeamAlbum dbTeamAlbum = teamAlbumService.getAlbum(albumId);
-        String url = storageProvider.putImage(
-                String.format("teams/%s/albums/%s", teamId, albumId),
-                image
-        );
+        return transactor.call(session -> {
+                    DbTeamAlbum dbTeamAlbum = teamAlbumService.getAlbum(albumId);
+                    String url = storageProvider.putImage(
+                            String.format("teams/%s/albums/%s", teamId, albumId),
+                            image
+                    );
 
-        DbTeamImage dbTeamImage = new DbTeamImage(
-                url,
-                dbTeamAlbum
+                    return session.save(
+                            new DbTeamImage(
+                                    url,
+                                    dbTeamAlbum
+                            )
+                    );
+                }
         );
-
-        return teamImageRepository.save(dbTeamImage);
     }
 }
