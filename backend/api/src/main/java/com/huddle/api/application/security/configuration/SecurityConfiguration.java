7a8a0967997ba873c1a18e44e3acec5c62cb1e67
Soham Parmar <soham.parmar@faire.com>
2023-06-29 19:42:51 -0600

    Static content 403s weirdly

--- backend/api/src/main/java/com/huddle/api/application/security/configuration/SecurityConfiguration.java
+++ backend/api/src/main/java/com/huddle/api/application/security/configuration/SecurityConfiguration.java
@@ -88,6 +88,10 @@ public class SecurityConfiguration {
                         authTokenFilter,
                         UsernamePasswordAuthenticationFilter.class
                 )
+                .addFilterBefore(
+                        staticContentFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
     }
 
